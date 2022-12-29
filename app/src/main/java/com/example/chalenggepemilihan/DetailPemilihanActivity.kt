d758690b29f0c97ab3fae354d3883a7d35873f71
dvaa <93278631+Dava-033@users.noreply.github.com>
2022-12-29 21:00:23 +0700

    Update Visibility

--- app/src/main/java/com/example/chalenggepemilihan/DetailPemilihanActivity.kt
+++ app/src/main/java/com/example/chalenggepemilihan/DetailPemilihanActivity.kt
@@ -11,7 +11,8 @@ import android.widget.LinearLayout
 class DetailPemilihanActivity : AppCompatActivity() {
     private lateinit var btnData : Button
     private lateinit var btnHasil : Button
-    private lateinit var textHide : LinearLayout
+    private lateinit var lyDataPemilih : LinearLayout
+    private lateinit var lyHasilPemilihan : LinearLayout
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,17 +20,43 @@ class DetailPemilihanActivity : AppCompatActivity() {
 
         btnData = findViewById(R.id.btnDataPemilih)
         btnHasil = findViewById(R.id.btnHasilPemilihan)
-        textHide = findViewById(R.id.textHide)
+        lyDataPemilih = findViewById(R.id.lyDataPemilih)
+        lyHasilPemilihan = findViewById(R.id.lyHasilPemilihan)
 
-//        btnData.setOnClickListener{
-//            val visibility = View.GONE
-//            val intent = Intent(this, )
-//            startActivity(textHide)
-//
-//
-//        }
-//         Hide
-// Show
-//        view.visibility = View.VISIBLE
+        var temp = false
+        btnData.setOnClickListener {
+            if(!temp){
+                lyHasilPemilihan.visibility = View.GONE
+//                Toast.makeText(applicationContext, "Invisible", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                lyDataPemilih.visibility = View.VISIBLE
+//                Toast.makeText(applicationContext, "Visible", Toast.LENGTH_SHORT).show()
+            }
+            temp = !temp
+        }
+
+        btnHasil.setOnClickListener {
+            if(!temp){
+                lyDataPemilih.visibility = View.GONE
+//                Toast.makeText(applicationContext, "Invisible", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                lyHasilPemilihan.visibility = View.VISIBLE
+//                Toast.makeText(applicationContext, "Visible", Toast.LENGTH_SHORT).show()
+            }
+            temp = !temp
+        }
+
+    }
+    fun View.setVisible(visible: Boolean) {
+        visibility = if (visible) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+    }
+    fun View.isVisible(): Boolean {
+        return visibility == View.VISIBLE
     }
 }
